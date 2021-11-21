f1a45d17c75737cec3917c9b6120185544c4c58a
Aina Jefelina Nuur <ainajefelina@student.ub.ac.id>
2021-11-21 21:23:10 +0700

    sip

---
+++ FP1/Uber Lasso Deployment/model.py
@@ -0,0 +1,32 @@
+# import pustaka
+import pandas as pd
+from pandas import read_csv
+from numpy import arange
+from sklearn.linear_model import LassoCV
+from sklearn.model_selection import RepeatedKFold
+from sklearn.model_selection import train_test_split
+import pickle
+
+# memuat dataset
+df=pd.read_csv('uber.csv')
+
+print(df.head())
+
+# memilih variabel independen dan dependen
+X =df[[ 'jarak','UberXL', 'Black', 'UberX','WAV', 'Black SUV', 'UberPool']]
+y =df['harga']
+
+# memisahkan dataset menjadi test dan uji
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=325)
+
+#cv
+cv = RepeatedKFold(n_splits=10, n_repeats=3, random_state=1)
+
+#definisi model
+lasso= LassoCV(cv=cv)
+
+# fit model
+lasso.fit(X_train, y_train)
+
+#model pickle
+pickle.dump(lasso, open('model.pkl', 'wb'))
